ecb63a29c8fa636e42d231c1d4727cee661ef318
Brendon W. <bwherry@midland.k12.ia.us>
2025-10-28 12:01:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+let distanceInMiles = 100;
+
+
+function estimatedArrivalTime(){
+let milesPerHour = 50;
+  return distanceInMiles / milesPerHour;
+}
+
+
+document.getElementById("result").innerHTML = estimatedArrivalTime;
+
+
+
+
+
+
+
+
+
+
 
